6fc30344b9c4272ed7ed8c30e96f52602e91eced
AbdullohBahar <abdullohbahar@gmail.com>
2022-05-23 10:44:54 +0700

    web pro pertemuan 7

--- application/controllers/Pinjam.php
+++ application/controllers/Pinjam.php
@@ -65,12 +65,15 @@ class Pinjam extends CI_Controller
         $this->ModelPinjam->simpanDetail($id_booking, $no_pinjam);
         $denda = $this->input->post('denda', TRUE);
         $this->db->query("update detail_pinjam set denda='$denda'");
-        //hapus Data booking yang bukunya diambil untuk dipinjam
+        //hapus Data booking yang bukunya dia+-mbil untuk dipinjam
         $this->ModelPinjam->deleteData('booking', ['id_booking' => $id_booking]);
         $this->ModelPinjam->deleteData('booking_detail', ['id_booking' => $id_booking]);
         //$this->db->query("DELETE FROM booking WHERE id_booking='$id_booking'");
+
         //update dibooking dan dipinjam pada tabel buku saat buku yang dibooking diambil untuk dipinjam
-        $this->db->query("UPDATE buku, detail_pinjam SET buku.dipinjam=buku.dipinjam+1, buku.dibooking=buku.dibooking-1 WHERE buku.id=detail_pinjam.id_buku");
+        $this->db->query("UPDATE buku, detail_pinjam SET buku.dipinjam=buku.dipinjam+1, buku.dibooking=buku.dibooking-1 WHERE buku.id=detail_pinjam.id_buku and no_pinjam='$no_pinjam'");
+        //update dibooking dan dipinjam pada tabel buku saat buku yang dibooking diambil untuk dipinjam
+        // $this->db->query("UPDATE buku, detail_pinjam SET buku.dipinjam=buku.dipinjam+1, buku.dibooking=buku.dibooking-1 WHERE buku.id=detail_pinjam.id_buku");
         $this->session->set_flashdata('pesan', '<div class="alert alert-message alert-success" role="alert">Data Peminjaman Berhasil Disimpan</div>');
         redirect(base_url() . 'pinjam');
     }
